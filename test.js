f85e53ccc4ad4cdc3ff9d315dbc2fbce168a497b
Rahul Prabhu <rahulprabhu@Rahuls-MacBook-Pro.local>
2019-02-21 14:34:54 +0530

    test

--- test.js
+++ test.js
@@ -1,19 +1,7 @@
-
-
-
 const Web3 = require('web3');
 const provider = new Web3.providers.HttpProvider('http://134.209.15.110:22000');
 const web3 = new Web3(provider);
-
-var raw = process.argv[2];
-console.log(raw);
-
-// const receipt = web3.eth.getTransactionReceipt('0x9a7576cf536c4b934342358303da1fdb26936051cf5c8b2964343b1cf5aad318')
-// .then((res) => {
-// 	console.log(res);
-// });
-
-
+var Tx = require('ethereumjs-tx');
 
 const contractAddress = '0xa4ceef94c7576197ebd9be150f5aa82482815d5b';
 const contractAbi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"value","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"value","type":"uint256"}],"name":"burn","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"isOwner","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"account","type":"address"}],"name":"isMinter","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"tokenAddress","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferAnyERC20Token","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_symbol","type":"string"},{"name":"_name","type":"string"},{"name":"_decimals","type":"uint256"},{"name":"totalSupply","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"tokenOwner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Approval","type":"event"}];
@@ -21,87 +9,40 @@ const contractAbi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"nam
 
 const contractInstance = new web3.eth.Contract(contractAbi, contractAddress);
 
-// contractInstance.methods.balanceOf('0x8EB3CB5D05eb889CD1510548485475D919A4b2A6').call({from: '0x8EB3CB5D05eb889CD1510548485475D919A4b2A6'}).then((result) => {
-// 	console.log(result);
-// });
 
 
-web3.eth.sendSignedTransaction(raw)
-.on('receipt', (result) => {
-	
-	
-	
-        var transactionHash = result.logs[0].transactionHash;
-     
-        console.log(transactionHash);
-    }).catch((err) => {
-        console.log(err);
-});
 
+const account = '0x8EB3CB5D05eb889CD1510548485475D919A4b2A6';
+privateKeys = '4167986A2B528A56DFB8E283A7606A511303102A706AF6FB23062FFF11BFB0C3';
+const privateKey = Buffer.from(privateKeys, 'hex');
 
+var accountDetails = web3.eth.accounts.create();
 
+let account2 = '0xaE3D415FE5488C4426A956eC53F1EA45BA181Fe1';
+let privateKey2 = accountDetails.privateKey;
 
 
+web3.eth.getTransactionCount(account, (err, txCount) => {
 
+  
 
+  const txObject = {
+  	from: account,
+    nonce:    web3.utils.toHex(txCount),
+    gas: web3.utils.toHex(4000000), 
+    to: contractAddress,
+    data: contractInstance.methods.transfer(account2, 0).encodeABI()
+  }
 
+  const tx = new Tx(txObject)
+  tx.sign(privateKey)
 
+  const serializedTx = tx.serialize()
+  const raw = '0x' + serializedTx.toString('hex')
 
+  console.log(raw);
 
+  });
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// console.log(web3);
-
-// web3.eth.getTransactionReceipt('0xb4f1d7450ae97829ee07f0b03a8d112af221e0531174fe02b1f30af667bc0591')
-// .then((result) => {
-// 	console.log(result);
-// });
